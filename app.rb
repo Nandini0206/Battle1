467e8528e93c7eea0f06807198715cc44d937ee9
shektor <44544977+shektor@users.noreply.github.com>
2019-04-18 15:18:30 +0100

    Use global variable temporarily

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
-
-
 require 'sinatra/base'
-require 'Player'
+require './lib/player'
 
 class Battle < Sinatra::Base
   set :session_secret, 'super secret'
@@ -12,14 +10,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    player_1 = Player.new(params[:player1])
-    player_2 = Player.new(params[:player2])
+    $player_1 = Player.new(params[:player1])
+    $player_2 = Player.new(params[:player2])
     redirect "/play"
   end
 
   get '/play' do
-    @player1 = player_1.return_name
-    @player2 = player_2.return_name
+    @player1 = $player_1.return_name
+    @player2 = $player_2.return_name
     erb(:play)
   end
 
